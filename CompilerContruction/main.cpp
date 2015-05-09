2dea5c1d6b98b2c995b1c60b8c941ac17ee751f2
cptiglu <cptiglu@cptiglu-ThinkPad-T60>
2015-05-09 15:53:33 +0200

    Erster Code commit ist nur ne Main

---
+++ CompilerContruction/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: cptiglu
+ *
+ * Created on 9. Mai 2015, 15:52
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
